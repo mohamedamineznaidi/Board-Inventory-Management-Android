4bd8ad9a820580c9001d92cbcac785eaaa7821ce
Mohamed Amine Znaidi <mohamedamine.znaidi@esprit.tn>
2017-07-29 17:53:52 +0100

    st_a

--- app/src/main/java/com/inventory/st/stboardinventory/Home.java
+++ app/src/main/java/com/inventory/st/stboardinventory/Home.java
@@ -2,6 +2,9 @@ package com.inventory.st.stboardinventory;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button home_bt = (Button) findViewById(R.id.home_bt);
+        home_bt.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "ay maadech tezel aliya", Toast.LENGTH_LONG).show();
 
+                    }
+                }
+        );
     }
 }
